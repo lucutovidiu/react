d13fe40e1a9ad561919bfd0f4f1e82bef6f4507d
lucutovidiu <48522713+lucutovidiu@users.noreply.github.com>
2019-03-13 16:34:36 +0200

    Add files via upload

---
+++ precache-manifest.b7ca46dd2e9933ce7017914682cd3d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "221df91c38360be74dd4",
+    "url": "/static/js/main.e08fd3f0.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "221df91c38360be74dd4",
+    "url": "/static/css/main.7d60ffad.chunk.css"
+  },
+  {
+    "revision": "2c6426038696e27998eff78d053f4848",
+    "url": "/index.html"
+  }
+];

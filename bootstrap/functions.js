21a2bc4d5d952e66fcd2853fa1366869cfa6b17b
lucutovidiu <48522713+lucutovidiu@users.noreply.github.com>
2019-03-19 08:59:38 +0200

    version 1

---
+++ bootstrap/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    $('.dropdown').on('show.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown("slow");
+    });
+    $('.dropdown').on('hide.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+}); 
